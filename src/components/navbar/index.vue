95e4f188460a8782bd419462be3bb01a91daf535
valentichu <valentichu@foxmail.com>
2022-05-09 15:53:57 +0800

    完成navbar

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -2,69 +2,36 @@
   <div class="navbar">
     <div class="left-side">
       <a-space>
-        <a-typography-title
-          :style="{ margin: 0, fontSize: '18px' }"
-          :heading="5"
-        >
+        <a-typography-title :style="{ margin: 0, fontSize: '18px' }" :level="5">
           Nest Vue Admin
         </a-typography-title>
       </a-space>
     </div>
-    <ul class="right-side">
-      <li>
-        <a-dropdown trigger="click">
-          <a-avatar
-            :size="32"
-            :style="{ marginRight: '8px', cursor: 'pointer' }"
-          >
-          </a-avatar>
-          <template #content>
-            <!-- <a-doption>
-              <a-space @click="switchRoles">
-                <icon-tag />
-                <span>
-                  切换角色
-                </span>
-              </a-space>
-            </a-doption>
-            <a-doption>
-              <a-space @click="$router.push({ name: 'info' })">
-                <icon-user />
-                <span>
-                  用户中心
-                </span>
-              </a-space>
-            </a-doption>
-            <a-doption>
-              <a-space @click="$router.push({ name: 'setting' })">
-                <icon-settings />
-                <span>
-                  用户设置
-                </span>
-              </a-space>
-            </a-doption> -->
-            <a-doption>
+    <div class="right-side">
+      <a-dropdown :trigger="['click']">
+        <a-avatar :size="32" :style="{ marginRight: '16px', cursor: 'pointer', backgroundColor: '#87d068' }">
+          <template #icon>
+            <user-outlined />
+          </template>
+        </a-avatar>
+        <template #overlay>
+          <a-menu>
+            <a-menu-item>
               <a-space @click="handleLogout">
-                <icon-export />
+                <export-outlined />
                 <span> 登出 </span>
               </a-space>
-            </a-doption>
-          </template>
-        </a-dropdown>
-      </li>
-    </ul>
+            </a-menu-item>
+          </a-menu>
+        </template>
+      </a-dropdown>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed } from "vue";
-import { useAppStore, useUserStore } from "@/stores";
 import useUser from "@/hooks/user";
-
-const appStore = useAppStore();
-const userStore = useUserStore();
 const { logout } = useUser();
-const avatar = "";
 const handleLogout = () => {
   logout();
 };
@@ -79,38 +46,10 @@ const handleLogout = () => {
   border-bottom: 1px solid rgb(229, 230, 235);
 }
 
-.left-side {
+.left-side,
+.right-side {
   display: flex;
   align-items: center;
   padding-left: 20px;
-}
-
-.right-side {
-  display: flex;
-  padding-right: 20px;
-  list-style: none;
-  li {
-    display: flex;
-    align-items: center;
-    padding: 0 10px;
-  }
-
-  a {
-    color: var(--color-text-1);
-    text-decoration: none;
-  }
-  .nav-btn {
-    border-color: rgb(var(--gray-2));
-    color: rgb(var(--gray-8));
-    font-size: 16px;
-  }
-  .trigger-btn,
-  .ref-btn {
-    position: absolute;
-    bottom: 14px;
-  }
-  .trigger-btn {
-    margin-left: 14px;
-  }
 }
 </style>
